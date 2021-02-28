87d0a0c8cfb469d6fc291557771d792e6a08f037
Trung Hoang <hoangminhtrung1995@gmail.com>
2021-02-28 19:11:31 +0700

    Adding Add/Update/Delete function

--- models/models.go
+++ models/models.go
@@ -38,20 +38,20 @@ type (
 
 // ToString ...
 func (t Student) ToString() string {
-	return fmt.Sprintf("Student ID: %s, Name: %s, Class ID: %s, Teachers: %s, Comments: %s", t.ID, t.Name, t.Class, t.Teachers, t.Comments)
+	return fmt.Sprintf("Student[ID: %s, Name: %s, Class ID: %s, Teachers: %s, Comments: %s]", t.ID, t.Name, t.Class, t.Teachers, t.Comments)
 }
 
 // ToString ...
 func (t Teacher) ToString() string {
-	return fmt.Sprintf("Teacher ID: %s, Name: %s, Disciplines : %s, Comments: %s", t.ID, t.Name, t.Disciplines, t.Comments)
+	return fmt.Sprintf("Teacher[ID: %s, Name: %s, Disciplines : %s, Comments: %s]", t.ID, t.Name, t.Disciplines, t.Comments)
 }
 
 // ToString ...
 func (t Discipline) ToString() string {
-	return fmt.Sprintf("Discipline ID: %s, Name: %s, Number of lecture : %d, Number of exercise: %d, Commments: %s", t.ID, t.Name, t.NumberOfLecture, t.NumberOfExercise, t.Comments)
+	return fmt.Sprintf("Discipline[ID: %s, Name: %s, Number of lecture : %d, Number of exercise: %d, Commments: %s]", t.ID, t.Name, t.NumberOfLecture, t.NumberOfExercise, t.Comments)
 }
 
 // ToString ...
 func (t Class) ToString() string {
-	return fmt.Sprintf("Class ID: %s, Name: %s", t.Identifier, t.Comments)
+	return fmt.Sprintf("Class[ID: %s, Name: %s]", t.Identifier, t.Comments)
 }
--- repositories/student.go
+++ repositories/student.go
@@ -9,21 +9,26 @@ import (
 	model "github.com/trunghoangminh/schoolmanagement/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// Student repository interface
 type Repository interface {
 	FindAll() ([]model.Student, error)
 	Add(student model.Student) error
 	Update(student model.Student) error
 	Delete(id string) error
 }
+
+// Student repositoru struct
 type StudentRepository struct {
-	col *mongo.Collection
-	ctx context.Context
+	collection *mongo.Collection
+	ctx        context.Context
 }
 
+// Find all Student
 func (studentRepos StudentRepository) FindAll() ([]model.Student, error) {
-	cursor, err := studentRepos.col.Find(studentRepos.ctx, bson.M{})
+	cursor, err := studentRepos.collection.Find(studentRepos.ctx, bson.M{})
 	if err != nil {
 		fmt.Println("Failed to get all student!")
 		log.Fatal(err)
@@ -37,13 +42,38 @@ func (studentRepos StudentRepository) FindAll() ([]model.Student, error) {
 	return student, nil
 }
 
+// Add new Student
+func (studentRepos StudentRepository) Add(student model.Student) error {
+	document, err := studentRepos.collection.InsertOne(studentRepos.ctx, student)
+	fmt.Print(document)
+	return err
+}
+
+// Update Student
+func (studentRepos StudentRepository) Update(student model.Student) error {
+	filter := bson.D{{"id", student.ID}}
+	after := options.After
+	returnOpt := options.FindOneAndUpdateOptions{ReturnDocument: &after}
+	update := bson.D{{"$set", bson.D{{"name", student.Name}}}}
+	return studentRepos.collection.FindOneAndUpdate(studentRepos.ctx, filter, update, &returnOpt).Err()
+}
+
+// Delete Student
+func (studentRepos StudentRepository) Delete(id string) error {
+	return studentRepos.collection.FindOneAndDelete(studentRepos.ctx, bson.D{{"id", id}}).Err()
+}
+
 func main() {
 	connection := database.ConnectMongoDB()
 	studentRepository := StudentRepository{connection.Collection("Student"), context.TODO()}
-	students, err := studentRepository.FindAll()
-	if err == nil {
-		for _, student := range students {
-			fmt.Println(student.ToString())
-		}
-	}
+	// students, err := studentRepository.FindAll()
+	// if err == nil {
+	// 	for _, student := range students {
+	// 		fmt.Println(student.ToString())
+	// 	}
+	// }
+	student := model.Student{"5303201", "Hoang Minh TrungS", "1305031", []string{"123", "456"}, []string{"123", "456"}}
+	// // studentRepository.Add(student)
+	// studentRepository.Delete("5303201")
+	studentRepository.Update(student)
 }
